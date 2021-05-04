ab1381440a69ac48f24a6c13311ab848e2c7ba02
klamb95 <klamb1995@gmail.com>
2021-05-04 17:28:19 +0100

    park set up tests passing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,15 +4,34 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+  let park;
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
+
   beforeEach(function () {
+    park = new Park('Kieran Park', 2);
+    dinosaur1 = new Dinosaur('T-rex', 'Carnivore', 50);
+    dinosaur2 = new Dinosaur('Velociraptor', 'Carnivore', 20);
+    dinosaur3 = new Dinosaur('T-triceratops', 'Herbivore', 10);
 
-  })
+  });
 
-  it('should have a name');
+  it('should have a name', function (){
+    const actual = park.name;
+    assert.strictEqual(actual, "Kieran Park");
 
-  it('should have a ticket price');
+  });
 
-  it('should have a collection of dinosaurs');
+  it('should have a ticket price', function (){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 2);
+  });
+
+  it('should have a collection of dinosaurs', function (){
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [])
+  });
 
   it('should be able to add a dinosaur to its collection');
 
